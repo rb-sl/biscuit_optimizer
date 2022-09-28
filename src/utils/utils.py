a60400dc4f83867b70240352b5a2dddd59ac3cf5
Roberto Basla <roberto.basla@mail.polimi.it>
2022-09-28 19:49:24 +0200

    Added greedy heuristics
    Updated the report, added bibliography and image for heuristic simulations.
    
    Renamed complete -> nesting for full models.
    
    Added the heuristics notebook.
    
    Added functions to utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -297,3 +297,138 @@ def can_host(base_mask: np.array, cutter_mask: np.array, n: int, m: int) -> bool
 	# Overlaps the two masks; if base_mask can entirely host cutter_mask, the result equals cutter_mask
 	masks_and = np.logical_and(base_mask[n:n+cutter_mask.shape[0], m:m+cutter_mask.shape[1]], cutter_mask)
 	return np.array_equal(masks_and, cutter_mask)
+
+
+def compute_feasible_y(dough_mask: np.array, cutters: dict[int, dict[str, Any]]) -> np.array:
+	"""
+	Computes which (n, m) can host a cutter for each cutter type.
+	Args:
+		dough_mask: The starting mask
+		cutters: The problem's cutters
+	Returns:
+		An np array of bools with one set of feasible (n, m) for each cutter
+	"""
+	y = np.zeros((len(cutters), *dough_mask.shape))
+	for i, cutter in enumerate(cutters):
+		cutter_mask = cutter["mask"]
+		for n in range(dough_mask.shape[0]):
+			for m in range(dough_mask.shape[1]):
+				y[i, n, m] = can_host(dough_mask, cutter_mask, n, m)					
+	return y
+
+
+def update_feasible_y(current_y: np.array, occupancy_table: np.array, cutters: dict[int, dict[str, Any]], i: int,
+		n: int, m: int) -> np.array:
+	"""
+	Changes the y according to the placement of the given cutter
+	Args:
+		current_y: The current y np array
+		occupancy_table: Array of sets describing which ys are incompatible with the activation of a given (i, n, m)
+		cutters: The problem's cutters
+		i: The id of the placed cutter
+		n: The row where the cutter is placed
+		m: The column where the cutter is placed
+	Returns:
+		The updated y array
+	"""
+	cutter_mask = cutters[i]["mask"]
+	updated_y = np.copy(current_y)
+	for h in range(cutter_mask.shape[0]):
+		for k in range(cutter_mask.shape[1]):
+			if cutter_mask[h, k]:
+				for (j, l, o) in occupancy_table[n+h, m+k]:
+					updated_y[j, l, o] = False
+	return updated_y
+
+
+def mask_from_y(feasible_y, cutters):  # TODO check against y to z
+	full_mask = np.zeros(feasible_y.shape[1:])
+	for (i, n, m) in np.argwhere(feasible_y):
+		cutter_mask = cutters[i]["mask"]
+		full_mask[n:n+cutter_mask.shape[0], m:m+cutter_mask.shape[1]] = np.where(cutter_mask, True, 
+			full_mask[n:n+cutter_mask.shape[0], m:m+cutter_mask.shape[1]])
+	return full_mask
+
+
+def mask_usable(dough_mask: np.array, cutters: dict[int, dict[str, Any]]) -> np.array:
+	"""
+	Computes the parts of the dough_mask that can actually be used
+	Args:
+		dough_mask: The mask of the input dough
+		cutters: Dictionary of cutters
+	Returns:
+		usable_mask: Mask True where z variables can be activated
+	"""
+	full_mask = np.zeros(dough_mask.shape)
+	for n in range(dough_mask.shape[0]):
+		for m in range(dough_mask.shape[1]):
+			for i, cutter in enumerate(cutters):
+				cutter_mask = cutter["mask"]
+				if can_host(dough_mask, cutter_mask, n, m):
+					# Activates the parts of the mask covered by y[n, m, i]
+					full_mask[n:n+cutter_mask.shape[0], m:m+cutter_mask.shape[1]] = \
+						np.logical_or(full_mask[n:n+cutter_mask.shape[0], m:m+cutter_mask.shape[1]], cutter_mask)
+
+	return np.logical_and(dough_mask, full_mask)
+
+
+def compute_waste(mask_1: np.array, mask_2: np.array) -> int:
+	"""
+	Computes the number of wasted pixels in going from mask_1 to mask_2
+	Args:
+		mask_1: First mask to compare
+		mask_2: Second mask to compare
+	Returns:
+		The number of wasted pixels
+	"""
+	return np.count_nonzero(np.logical_xor(mask_1, mask_2))
+
+
+def compute_occupancy_table(dough_mask, cutters):
+	occupancy_table = [[set() for m in range(dough_mask.shape[1])] for n in range(dough_mask.shape[0])]
+	for i, cutter in enumerate(cutters): 
+		cutter_mask = cutter["mask"]
+		for n in range(dough_mask.shape[0]): 
+			for m in range(dough_mask.shape[1]): 
+				if can_host(dough_mask, cutter_mask, n, m):
+					for h in range(cutter_mask.shape[0]):
+						for k in range(cutter_mask.shape[1]):
+							if cutter_mask[h, k]:
+								occupancy_table[n+h][m+k].add((i, n, m))
+	return np.array(occupancy_table)
+
+
+def compute_value_cost(dough_mask, cutters):
+	return -np.stack([np.full_like(dough_mask, cutter["value"]) for cutter in cutters], axis=0)
+
+
+def compute_area_cost(dough_mask, cutters):
+	return np.stack([np.full_like(dough_mask, np.count_nonzero(cutter["mask"])) for cutter in cutters], axis=0)
+
+
+def compute_occupancy_cost(dough_mask, cutters, occupancy_table):
+	occupancy_cost_map = np.zeros((len(cutters), *dough_mask.shape))
+	for i in range(len(cutters)):
+		cutter_mask = cutters[i]["mask"]
+		for n in range(dough_mask.shape[0]):
+			for m in range(dough_mask.shape[1]):
+				if can_host(dough_mask, cutter_mask, n, m):
+					for h in range(cutter_mask.shape[0]):
+						for k in range(cutter_mask.shape[1]):
+							if cutter_mask[h, k]:
+								occupancy_cost_map[i, n, m] += len(occupancy_table[n+h, m+k])
+	occupancy_cost_map /= 10000
+	return occupancy_cost_map
+
+
+def show_cost_map(cost_map, i=0, cmap="cividis"):
+	flat_cost_map = np.sum(cost_map, axis=0)
+	print("(Flattened) Shape:", flat_cost_map.shape, "Min:", np.min(flat_cost_map), "Max:", np.max(flat_cost_map))
+	print(f"(Layer {i}) Shape:", cost_map[0].shape, "Min:", np.min(cost_map[0]), "Max:", np.max(cost_map[0]))
+
+	fig, ax = plt.subplots(1, 2, figsize=(8, 4))
+	ax[0].imshow(flat_cost_map, cmap=cmap)
+	ax[0].set_title("Flattened cost map")
+	ax[1].imshow(cost_map[0], cmap=cmap)
+	ax[1].set_title(f"Cost map layer {i}")
+	plt.show()
